36a0ccf92ecc25e9509a7e65f7c16d998742a167
Danilo Mendes <danilo@iMac-de-Ana.local>
2019-04-07 10:06:07 -0300

    WIP: Working scratch for using MKL from conda env

--- llckbdm/cpp/kbdm.cpp
+++ llckbdm/cpp/kbdm.cpp
@@ -1,10 +1,23 @@
 #include "kbdm.hpp"
+#include "mkl.h"
 #include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+
 
 
 int main() {
-    std::cout << hello("World") << std::endl;
+    auto X = Eigen::Matrix2d::Zero(2, 2).eval();
+    X(0, 0) = 1;
+    X(1, 1) = 1;
+    std::cout << X << std::endl;
+
+    Eigen::JacobiSVD<Eigen::Matrix2d> svd(X, Eigen::ComputeFullV);
+
+    std::cout << svd.computeV();
+
     return 0;
+
 }
 
 std::string hello(std::string name) {
