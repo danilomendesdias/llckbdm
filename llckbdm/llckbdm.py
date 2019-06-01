b3afb2001d96578f4930253095bc02f8ff0aac2f
danilomendesdias <danilomendesdias@gmail.com>
2019-06-01 14:21:32 -0300

    Fix default parameter q in iterative_llc_kbdm

--- llckbdm/llckbdm.py
+++ llckbdm/llckbdm.py
@@ -135,7 +135,7 @@ def llc_kbdm(data, dwell, m_range, p=1, l=None, q=0.0):
 
 
 def iterative_llc_kbdm(
-        data, dwell, m_range, p=1, l=None, q=None, max_iterations=5, silhouette_threshold=0.6
+        data, dwell, m_range, p=1, l=None, q=0.0, max_iterations=5, silhouette_threshold=0.6
 ):
     if max_iterations < 1:
         raise ValueError("'max_iterations must be greater than zero")
