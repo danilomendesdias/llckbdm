8d3f5c80579e416692f150c1524a738fd65e9420
danilomendesdias <danilomendesdias@gmail.com>
2020-08-29 11:09:22 -0300

    Release 0.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 def main():
     setup(
         name='llckbdm',  
-        version='0.2.1',
+        version='0.2.2',
         description='Line List Clustering Krylov Basis Diagonalization Method implementation in Python',
         long_description=long_description,  
         long_description_content_type='text/markdown',
